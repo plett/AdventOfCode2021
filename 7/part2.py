c5a76c8eb181d1643faf81ea63c773fdc3d3899c
Paul Lettington <paul@plett.co.uk>
2021-12-07 12:25:10 +0000

    Working day 7 solutions

---
+++ 7/part2.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+'''
+The crabs don't seem interested in your proposed solution. Perhaps you
+misunderstand crab engineering?
+
+As it turns out, crab submarine engines don't burn fuel at a constant rate.
+Instead, each change of 1 step in horizontal position costs 1 more unit of fuel
+than the last: the first step costs 1, the second step costs 2, the third step
+costs 3, and so on.
+
+As each crab moves, moving further becomes more expensive. This changes the best
+horizontal position to align them all on; in the example above, this becomes 5:
+
+  - Move from 16 to 5: 66 fuel
+  - Move from 1 to 5: 10 fuel
+  - Move from 2 to 5: 6 fuel
+  - Move from 0 to 5: 15 fuel
+  - Move from 4 to 5: 1 fuel
+  - Move from 2 to 5: 6 fuel
+  - Move from 7 to 5: 3 fuel
+  - Move from 1 to 5: 10 fuel
+  - Move from 2 to 5: 6 fuel
+  - Move from 14 to 5: 45 fuel
+
+This costs a total of 168 fuel. This is the new cheapest possible outcome; the
+old alignment position (2) now costs 206 fuel instead.
+
+Determine the horizontal position that the crabs can align to using the least
+fuel possible so they can make you an escape route! How much fuel must they
+spend to align to that position?
+'''
+
+import fileinput
+
+pos = []
+for line in fileinput.input():
+    pos = list(map(int,line.split(',')))
+
+
+def fuelcost(start, target) -> int:
+    dist = abs(start - target)
+    return int( (dist*(dist+1))/2 )
+
+bestpos=None
+bestfuel=None
+for i in range( min(pos) , max(pos) ):
+    fuel = sum( [fuelcost(p,i) for p in pos] )
+    #print(f"position {i} costs {fuel}")
+    if bestfuel and fuel>bestfuel:
+        #print("fuel costs rising, we are past minimum, abort")
+        break
+    if not bestfuel or fuel<bestfuel:
+        bestfuel=fuel
+        bestpos=i
+print(f"Best postition is {bestpos}, it costs {bestfuel}")
+
+
+
