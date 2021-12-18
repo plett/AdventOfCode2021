42fdde8481a9a71713295da1c7b7cc61d29638c5
Paul Lettington <paul@plett.co.uk>
2021-12-18 23:43:08 +0000

    Day 13 working solutions

---
+++ 13/part2.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+import fileinput
+import numpy as np
+import re
+
+def fold(grid, direction, pos):
+    if direction == 'y':
+        #folding "up"
+        newgrid = grid[ 0:pos , : ]
+        flip = np.flipud(grid[ pos+1:2*pos+1, : ])
+        newgrid += flip
+        return newgrid
+    elif direction == 'x':
+        #folding "across"
+        newgrid = grid[ : , 0:pos]
+        flip = np.fliplr(grid[ : , pos+1:2*pos+1 ])
+        newgrid += flip
+        return newgrid
+    else:
+        raise Exception("unknown direction")
+
+
+def main():
+    grid = np.full( (2000,2000), False, np.bool_)
+    folds = []
+    for line in fileinput.input():
+        m = re.search(r"^(\d+),(\d+)$",line)
+        if m != None:
+            x,y = map(int,m.groups())
+            grid[y][x] = True
+        else:
+            m = re.search(r"^fold along (\w+)=(\d+)$",line)
+            if m != None:
+                folds.append( ( m.groups()[0] , int(m.groups()[1]) ) )
+
+    for direction, pos in folds:
+        grid = fold(grid, direction, pos)
+
+    visualise = np.vectorize(lambda x: '#' if x else ' ')
+    listgrid = visualise(grid).tolist()
+    for row in listgrid:
+        print(''.join(row))
+
+if __name__ == '__main__':
+    main()
